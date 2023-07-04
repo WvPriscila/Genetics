f872f6d97d516da6a4d70a638534c3790a3733c5
faamii <68048874+faamii@users.noreply.github.com>
2023-07-03 21:06:03 -0300

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,1 +1,79 @@
+import random
+from math import *
+import matplotlib.pyplot  as pl
+
+
+
+global MÃE_de_todas
+global novo_gene
+global E
+global G
+global Primeiro_Homem
+
+
+def gene(Escolhida,escolhido):
+    G = (Escolhida[2] --escolhido[2])/2
+    E = random.uniform(-1, 1)
+    novo_gene = G - (G / E)
+    if novo_gene < -1 or novo_gene > 1:
+        novo_gene = sin(radians(novo_gene))
+    return novo_gene
+
+#-------------------------------------------------------------------------------#
+
+def Verifica_antepassada_comum(Escolhida,escolhido,lista_de_identificação):
+
+    geração_anterior_Escolhida = [Escolhida[5][0], Escolhida[5][1]] # ['gnr',G,Dna,id,[ g_F, g_M ], [ id_F, id_M ]]
+    geração_anterior_escolhido = [escolhido[5][0], escolhido[5][1]] # ['gnr',G,Dna,id,[ g_F, g_M ], [ id_F, id_M ]]
+    antepassado_em_comum = set(geração_anterior_Escolhida) & set(geração_anterior_escolhido)
+    A  = []
+    B  = []
+    Critério = True
+    print("Escolhida: ",Escolhida)
+    print("escolhido: ",escolhido)
+    print("lista_de_identificação: ", lista_de_identificação)
+    while True:
+
+        C = [lista_de_identificação[i-1] for i in antepassado_em_comum if i is not None]
+        Critério = False if any( i[0] == "M" and i[3] != 2 for i in C ) else True
+        return Critério if len(antepassado_em_comum) != 0 else  Critério
+
+        C = []
+        if len(antepassado_em_comum) != 0:
+            for i in antepassado_em_comum:
+                if i == None:
+                    continue
+                C.append(lista_de_identificação[i-1])
+            for i in C:
+                if i[0] == "M" and i[3] != 2:
+                    Critério = False
+                    return Critério
+        if len(antepassado_em_comum) == 0 or antepassado_em_comum == None:
+            return True
+        # Para a Escolhida
+        for i in geração_anterior_Escolhida: # ['gnr',G,Dna,id,[ g_F, g_M ], [ id_F, id_M ]]
+            if i == None:
+                continue
+            A.append(lista_de_identificação[i-1])
+        geração_anterior_Escolhida = []
+        for i in A:
+            if i == None:
+                continue
+            geração_anterior_Escolhida.append(i[5][0])
+            geração_anterior_Escolhida.append(i[5][1])
+        # Para o escolhido
+        for i in geração_anterior_escolhido:
+            if i == None:
+                continue
+            B.append(lista_de_identificação[i - 1])
+        geração_anterior_escolhido = []
+        for i in B:
+            if i == None:
+                continue
+            geração_anterior_escolhido.append(i[5][0])
+            geração_anterior_escolhido.append(i[5][1])
+        antepassado_em_comum = set(geração_anterior_Escolhida) & set(geração_anterior_escolhido)
+        return Critério
+
+
 
