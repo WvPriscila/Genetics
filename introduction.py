202adfaacb9e5f0008c111c14ab5ca299ba5e515
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-07-06 15:41:36 -0300

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -33,4 +33,50 @@ então espara-se poder dizer que a taxa média de mutação de  pares de bases e
 então se simularmos um cromossomo de 1000  pares de bases haveria uma mutação de aproximadamente 95 pares de bases em média.
 
 
+
+"""
+
+
+
+"""
+     Cada pessoa nesse algoritmo é representada da seginte forma:   [ A, B, C, D, [ E, F ], [ G, H ] ] 
+     
+     # De modo que 'A' é o gênero da pessoa
+     # De modo que 'B' é a geração da pessoa
+     # De modo que 'C' é o Dna da pessoa
+     # De modo que 'D' é a indentificação individual e única da pessoa
+     # De modo que 'E' é a geração da mãe da pessoa
+     # De modo que 'F' é a geração do pai da pessoa
+     # De modo que 'G' é a indentificação individual e única da mãe da pessoa
+     # De modo que 'H' é a indentificação individual e única do pai da pessoa
+
+"""
+
+"""    
+      O Dna de cada pessoa possui 3 pares de Cromossomos
+
+     # O primeiro Par de Cromossomos é chamado de 'Par A'
+     # O segundo Par de Cromossomos é chamado de 'Par B'
+     # O Terceiro Par de Cromossomos é chamado de 'Par C'
+
+     O Par A determina o gênero da pessoa
+     O Par B determina as Características físicas e psicologicas
+     O Par C determina determina as regras de reprodução e como e quais características seram passadas para geração seguinte
+
+     Cada pessoa herda 3 Cromossomos da mãe  e  3 cromossomos do pai formando assim 3 pares de Cromossomos
+      
+"""
+
+
+"""
+    O algoritmo  reproduz  seguindo as seguintes regras:
+
+    
+    # Um homem não pode se reproduzir com alguém que seja seu ou sua descendente
+    # Uma pessoa não pode se reproduzir com uma pessoa que seja parente de sangue de seu pai
+    # Um homem não pode se reproduzir com alguém que seja de uma geração posterior a dele
+    # Um homem não pode se reproduzir com mais de uma mulher
+    # Um homem não pode se reproduzir mais de uma vez
+    # Não há nascimento de gêmeos masculinos 
+    
 """
