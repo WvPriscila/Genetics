be6a40feabdf4de80d48148b56fd8597ccb56959
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-08-22 17:22:04 -0300

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -60,11 +60,6 @@ def População_inicial(a):
 
 
 
- 
-
- #-------------------------------------------------------------------------------#
-
-
 #-------------------------------------------------------------------------------#
 
 
@@ -158,3 +153,205 @@ def Verifica_antepassada_comum(Escolhida,escolhido,lista_de_identificação):
             geração_anterior_escolhido.append(i[5][1])
         antepassado_em_comum = set(geração_anterior_Escolhida) & set(geração_anterior_escolhido)
         return Critério
+
+#-----------------------------------------------------------#
+#-----------------------------------------------------------#
+
+def inicializar_populacao():
+    for i in range(tamanho_população_inicial):
+        individua = [random.randint(0, 9) for i in range(num_genes)]
+        POPULAÇÃO.append(individua)
+    return POPULAÇÃO
+
+# -----------------------------#
+# -----------------------------#
+
+
+def Unir_digitos(lista):
+    A = ''
+    sinal = int(lista[0])
+    for i in lista:
+        i = str(i)
+        A = A + i
+   
+    B = f"{A[1:digitos_inteiro + 1]}.{A[digitos_inteiro + 1:]}"
+
+    número = (-1) ** sinal *Decimal(B)
+    return número
+
+# -----------------------------#
+# -----------------------------#
+
+
+# Função de Aptidão
+def Aptidão(individua):
+    
+    x = Unir_digitos(individua)
+   
+    valor = Decimal(x**2)
+    aptidao = abs(valor - Decimal(objetivo)) 
+    
+
+    return aptidao
+    
+# -----------------------------#
+# -----------------------------#
+
+
+# Função para realizar a mistura entre dois indivíduos
+def Mistura(individua1, individua2):
+    
+    indi_1 = individua1.copy()
+    indi_2 = individua2.copy()
+    
+
+    for i in indi_1:
+        numero_aleatorio = random.random()
+        if numero_aleatorio < 0.5:
+            posição = random.randint(0, len(indi_1) - 1)
+            indi_2[posição] = i
+
+    for i in indi_2:
+        numero_aleatorio = random.random()
+        if numero_aleatorio < 0.5:
+            posição = random.randint(0, len(indi_2) - 1)
+            indi_1[posição] = i
+    
+    filha1, filha2 = indi_1,indi_2
+    
+    ponto_corte = random.randint(1, num_genes - 1)
+    filha3 = individua1[:ponto_corte] + individua2[ponto_corte:]
+    filha4 = individua2[:ponto_corte] + individua1[ponto_corte:]
+    
+    return filha1, filha2, filha3, filha4
+
+# -----------------------------#
+# -----------------------------#
+
+def Possivel_mutação(individua):
+    for i in range(len(individua)):
+        if random.random() < taxa_mutacao:
+            individua[i] = random.randint(0, 9)
+    return individua
+
+def mutação_Obrigatória(individua):
+    for i in range(len(individua)):
+        individua[i] = random.randint(0, 9)
+    return individua
+# -----------------------------#
+# -----------------------------#
+
+
+def seleção(populção):
+    total_aptidão = sum(Aptidão(individuo) for individuo in populção)
+    escolhido = random.uniform(0, float(total_aptidão))  # Convertendo para float
+    acumulado = 0
+
+    for individuo in populção:
+        aptidão_individuo = Aptidão(individuo)
+        acumulado += aptidão_individuo
+
+        if acumulado >= escolhido:
+            return individuo
+
+# -----------------------------#
+# -----------------------------#
+
+def minima(Z):
+    Z = POPULAÇÃO
+    for i in range(len(POPULAÇÃO)):
+        ind = (Unir_digitos(POPULAÇÃO[i]))
+        individuas.append(ind)
+        A = Aptidão(ind)
+        Aptidão.appen(A)
+#-----------------------------#
+# -----------------------------#
+def encontrar_menor_valor(lista):
+    if not lista:  # Se a lista estiver vazia
+        return None
+
+    menor = lista[0]  # Inicializa com o primeiro valor da lista
+
+    for valor in lista:
+        if valor < menor:
+            menor = valor
+
+    return menor
+ #------------------------------#
+
+
+def encontrar_maior_elemento(lista):
+    maior = lista[0]
+    for elemento in lista:
+        if elemento > maior:
+            maior = elemento
+    return maior
+
+def ordenacao_decrescente(lista):
+    lista_ordenada = []
+    while lista:
+        maior_elemento = encontrar_maior_elemento(lista)
+        lista_ordenada.append(maior_elemento)
+        lista.remove(maior_elemento)
+    return lista_ordenada
+#------------------------------#
+#------------------------------#
+
+def encontrar_menor_elemento(lista):
+    menor = lista[0]
+    for elemento in lista:
+        if elemento < menor:
+            menor = elemento
+    return menor
+
+def ordenacao_crescente(lista):
+    lista_ordenada = []
+    while lista:
+        menor_elemento = encontrar_menor_elemento(lista)
+        lista_ordenada.append(menor_elemento)
+        lista.remove(menor_elemento)
+    return lista_ordenada
+#------------------------------#
+#------------------------------#
+
+def separadora(arrey):
+
+    if len(arrey) % 2 == 0:
+        indice = int(len(arrey) / 2)
+    else:
+        indice = int(len(arrey) // 2 --1)
+
+    return indice
+#------------------------------#
+#------------------------------#
+nova_população = []
+    
+def Reprodução(mãe1, mãe2):
+    
+    global nova_população 
+    global filha1, filha2, filha3, filha4
+    global filha5, filha6,filha7, filha8
+    global filha9, filha10, filha11, filha12
+  
+        
+    filha1, filha2, filha3, filha4 = Mistura(mãe1, mãe2)
+
+    nova_população = nova_população + [filha1, filha2,filha3,filha4]
+            
+    filha5, filha6 = filha1.copy(), filha2.copy()
+    filha7, filha8 = filha3.copy(), filha4.copy()
+            
+     mutação_Obrigatória(filha5)
+     mutação_Obrigatória(filha6)
+     mutação_Obrigatória(filha7)
+     mutação_Obrigatória(filha8)
+
+     nova_população = nova_população + [filha5, filha6]
+     nova_população = nova_população + [filha7, filha8]
+       
+     filha9  = Possivel_mutação(filha1)
+     filha10 = Possivel_mutação(filha2)
+     filha11 = Possivel_mutação(filha3)
+     filha12 = Possivel_mutação(filha4)
+
+     nova_população = nova_população + [filha9, filha10, filha11, filha12]
